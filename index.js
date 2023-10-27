4dcb7c9b3db22969fe68c67b31d56d4d3b65ebf4
Mahfujur Rahman <mahfujurrahman6793@gmail.com>
2023-10-27 22:42:38 +0600

    added a basic modal

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const handleLoad = async (id) => {
   const data = await res.json();
   specifiedData = data.data;
   console.log(specifiedData);
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = '';
-    
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+
   specifiedData.map((item) => {
     const section = document.createElement("section");
     section.innerHTML = `
@@ -61,7 +61,7 @@ const handleLoad = async (id) => {
           </div>
         </div>
         <div class="card-detaild-btn">
-          <button
+          <button onclick='handleModal("${item._id}")'
             class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
           >
             Details
@@ -71,9 +71,53 @@ const handleLoad = async (id) => {
     </div>
   </div>
         `;
-        
+
     cardContainer.appendChild(section);
   });
 };
+
+const handleModal = async (id) => {
+  console.log(id);
+    const modalContainer = document.getElementById("modal-container");
+    modalContainer.innerHTML = '';
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/${id}`
+  );
+    const data = await res.json();
+    const news = data.data;
+    //   console.log(data.data);
+    news.map((specificnews) => {
+        const div = document.createElement("div");
+  div.innerHTML = `
+    
+
+<dialog id="my_modal_1" class="modal">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">${specificnews.title.slice(0,20)}</h3>
+    <div>
+        <figure>
+            <img src=${specificnews.image_url}/>
+        </figure>
+    </div>
+    <p class="py-4">${specificnews.details}</p>
+    <div class="modal-action">
+      <form method="dialog">
+       
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+    `;
+    modalContainer.appendChild(div);
+    const modal = document.getElementById('my_modal_1');
+    modal.showModal();
+    
+    })
+
+  
+
+
+};
 handleCategory();
-handleLoad('01');
+handleLoad("01");
