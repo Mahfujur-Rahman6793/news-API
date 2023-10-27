5079b1d0b1ea7e8672f30be7dc6b587328acb515
Mahfujur Rahman <mahfujurrahman6793@gmail.com>
2023-10-27 18:59:29 +0600

    showing the first three category

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const handleCategory = async() => {
+const handleCategory = async () => {
+    const tabContainer = document.getElementById('tab-container');
     const res =await fetch(`https://openapi.programming-hero.com/api/news/categories`);
     const data = await res.json();
     console.log(data);
     const categoryNameDetails = data.data.news_category;
+    categoryNameDetails.slice(0,3).forEach((category) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="tab">${category['category_name']}</a>
+        `;
+        tabContainer.appendChild(div);
+    })
     console.log(categoryNameDetails);
 }
 handleCategory();
